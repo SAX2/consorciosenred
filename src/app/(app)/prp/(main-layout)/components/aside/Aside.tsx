017c18c2f789ed9bca3bf0948bf1ff25ea29b607
SAX2 <santinodegra73@gmail.com>
2024-08-06 15:26:57 -0300

    Arreglo de [REDIRECCION ATAJOS NO VISIBLE]

--- src/app/(app)/prp/(main-layout)/components/aside/Aside.tsx
+++ src/app/(app)/prp/(main-layout)/components/aside/Aside.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { usePathname } from 'next/navigation'
+import { useParams } from 'next/navigation'
 import React from 'react'
 import AsideUnit from './AsideUnit'
 import AsideMain from './AsideMain'
@@ -10,12 +10,12 @@ interface AsideProps {
 }
 
 const Aside: React.FC<AsideProps> = ({ children }) => {
-  const pathname = usePathname();
+  const params = useParams();
 
   return (
     <aside className="flex flex-col gap-8 w-full col-start-3 min-w-[400px] max-xl:min-w-[300px] max-lg:min-w-[250px] max-md:min-w-fit max-md:col-start-1 sticky top-60">
       {children}
-      {pathname === `/prp/expensas` ? <AsideMain /> : <AsideUnit />}
+      {params.id ? <AsideUnit /> : <AsideMain />}
     </aside>
   );
 }

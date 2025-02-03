3f9f725fb305ae0cd83efa014d5fffc4a978d121
SAX2 <santinodegra73@gmail.com>
2025-02-03 20:42:21 -0300

    Añade boton de refresh a componente EmptySection

--- src/components/Sections/AppSections/Errors/EmptySection/index.tsx
+++ src/components/Sections/AppSections/Errors/EmptySection/index.tsx
@@ -1,10 +1,15 @@
-import { IconPlaylistX, IconProps } from '@tabler/icons-react'
+"use client"
+
+import Button from '@/components/Buttons/Button';
+import { IconPlaylistX, IconProps, IconRefresh, IconReload } from '@tabler/icons-react'
+import { useRouter } from 'next/navigation';
 import React, { FC } from 'react'
 
 interface EmptySection {
   title?: string;
   description?: string;
   Icon?: React.ElementType<IconProps>;
+  reloadButton?: boolean;
 }
 
 let DEFAULT_TITLE = "No hay resultados disponibles";
@@ -14,13 +19,25 @@ const EmptySection: FC<EmptySection> = ({
   Icon = IconPlaylistX,
   description = DEFAULT_DESC,
   title = DEFAULT_TITLE,
+  reloadButton = false,
 }) => {
+
+  const router = useRouter()
+
   return (
-    <div className='w-full flex justify-center'>
+    <div className="w-full flex justify-center">
       <div className="flex flex-col gap-[10px] max-w-[350px] w-full items-center">
         <Icon size={44} className="text-black" />
         <h3 className="font-semibold text-xl text-center">{title}</h3>
         <p className="text-text-grey text-center">{description}</p>
+        {reloadButton && (
+          <Button
+            onClick={() => router.refresh()}
+            title="Volver a cargar"
+            icon={<IconRefresh size={24} className="text-text-grey"/>}
+            classNameText="text-text-grey"
+          />
+        )}
       </div>
     </div>
   );

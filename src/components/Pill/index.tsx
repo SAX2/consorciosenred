03f2292924e44d1048b1d70e6aea45f72281ebdb
SAX2 <santinodegra73@gmail.com>
2025-02-04 15:38:00 -0300

    Hace cambios en el componente Pill en la logica de variant

--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -15,6 +15,9 @@ export interface PillProps {
   variant?: StatusValue;
 }
 
+export const variantContainerClassName = (variant: StatusValue) => variant === "warning" ? "border-yellow-sec/15 bg-yellow/15" : variant === "error" ? "border-red/25 bg-red/15" : variant === "success" ? "border-green/15 bg-green/15" : variant === "neutral" ? "border-outline bg-grey-sec" : variant === "default" ? "border-outline bg-white" : variant === "info" ? "bg-blue-button/15 border-blue-button/15": "" 
+export const variantTextClassName = (variant: StatusValue) => variant === "warning" ? "text-yellow-sec" : variant === "error" ? "text-red" : variant === "success" ? "text-green" : variant === "neutral" ? "text-text-grey" : variant === "default" ? "text-black" : variant === "info" ? "text-blue-button": ""
+
 const Pill: React.FC<PillProps> = ({
   text,
   className,
@@ -25,9 +28,6 @@ const Pill: React.FC<PillProps> = ({
   iconOrientation = "left",
   variant = "default"
 }) => {
-  const variantContainerClassName = variant === "warning" ? "border-yellow-sec/15 bg-yellow/15" : variant === "error" ? "border-red/25 bg-red/15" : variant === "success" ? "border-green/15 bg-green/15" : variant === "neutral" ? "border-outline bg-grey-sec" : variant === "default" ? "border-outline bg-white" : "" 
-  const variantTextClassName = variant === "warning" ? "text-yellow-sec" : variant === "error" ? "text-red" : variant === "success" ? "text-green" : variant === "neutral" ? "text-text-grey" : variant === "default" ? "text-black" : ""
-
   if (isFile) {
     return (
       <Link
@@ -57,14 +57,14 @@ const Pill: React.FC<PillProps> = ({
   return (
     <div
       className={cn(
-        variantContainerClassName,
+        variantContainerClassName(variant),
         "darkk:bg-grey-sec-dark darkk:border-outline-dark flex w-fit items-center justify-center rounded-[6px] border px-1 text-xs font-medium flex-row",
         className
       )}
     >
       {iconOrientation === "left" && icon}
       <span
-        className={cn("dark:text-white font-medium", classNameText, variantTextClassName)}
+        className={cn("dark:text-white font-medium", classNameText, variantTextClassName(variant))}
       >
         {text}
       </span>

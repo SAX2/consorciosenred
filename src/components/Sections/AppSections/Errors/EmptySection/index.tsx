d2277bc8cc49734d1659df1e8edb30196f21710b
SAX2 <santinodegra73@gmail.com>
2025-02-04 15:39:01 -0300

    Añade boton para refrescar pagina o la opcion de customButton en componente EmptySection

--- src/components/Sections/AppSections/Errors/EmptySection/index.tsx
+++ src/components/Sections/AppSections/Errors/EmptySection/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Button from '@/components/Buttons/Button';
-import { IconPlaylistX, IconProps, IconRefresh, IconReload } from '@tabler/icons-react'
+import Button, { ButtonProps } from '@/components/Buttons/Button';
+import { IconPlaylistX, IconProps, IconRotateClockwise2 } from '@tabler/icons-react'
 import { useRouter } from 'next/navigation';
 import React, { FC } from 'react'
 
@@ -9,35 +9,43 @@ interface EmptySection {
   title?: string;
   description?: string;
   Icon?: React.ElementType<IconProps>;
-  reloadButton?: boolean;
+  reload?: boolean;
+  button?: ButtonProps;
+  children?: React.ReactNode;
 }
 
 let DEFAULT_TITLE = "No hay resultados disponibles";
 let DEFAULT_DESC = "Parece que no hay elementos para mostrar en este momento. Prueba con otros criterios de búsqueda o agrega nuevos registros.";
+let DEFAULT_BTN_PROPS: ButtonProps = { classNameContainer: "max-w-[300px] w-full" }
 
 const EmptySection: FC<EmptySection> = ({
   Icon = IconPlaylistX,
   description = DEFAULT_DESC,
   title = DEFAULT_TITLE,
-  reloadButton = false,
+  reload = false,
+  button,
+  children
 }) => {
 
   const router = useRouter()
 
   return (
     <div className="w-full flex justify-center">
-      <div className="flex flex-col gap-[10px] max-w-[350px] w-full items-center">
+      <div className="flex flex-col gap-[10px] max-w-[425px] w-full items-center">
         <Icon size={44} className="text-black" />
         <h3 className="font-semibold text-xl text-center">{title}</h3>
         <p className="text-text-grey text-center">{description}</p>
-        {reloadButton && (
+        {reload && !button && (
           <Button
+            classNameContainer={DEFAULT_BTN_PROPS.classNameContainer}
             onClick={() => router.refresh()}
-            title="Volver a cargar"
-            icon={<IconRefresh size={24} className="text-text-grey"/>}
+            title="Volver a intentar"
+            icon={<IconRotateClockwise2 size={24} className="text-text-grey"/>}
             classNameText="text-text-grey"
           />
         )}
+        {reload && button && <Button {...button}/>}
+        {children}
       </div>
     </div>
   );

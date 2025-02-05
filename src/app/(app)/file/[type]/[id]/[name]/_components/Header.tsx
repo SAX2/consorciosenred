a23bfff6453a30c0d6efb62797ff7d8e82580e78
SAX2 <santinodegra73@gmail.com>
2025-02-05 16:41:54 -0300

    Añade soporte de imagenes en la ruta de archivos

---
+++ src/app/(app)/file/[type]/[id]/[name]/_components/Header.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Button from '@/components/Buttons/Button';
+import { IconChevronLeft, IconShare } from '@tabler/icons-react';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const Header = () => {
+  const router = useRouter()
+
+  return (
+    <div className="p-8 max-md:px-4 flex justify-between">
+      <Button
+        onClick={() => router.back()}
+        buttonPadding="p-2 px-0"
+        buttonBackground=""
+        classNameText="text-text-grey"
+        title="Volver atras"
+        icon={<IconChevronLeft size={24} className="text-text-grey" />}
+      />
+      <Button
+        buttonPadding="p-2"
+        buttonBackground="hover:bg-grey-dark"
+        icon={<IconShare size={24} className="text-text-grey" />}
+      />
+    </div>
+  );
+}
+
+export default Header
--- src/app/(app)/file/[type]/[id]/[name]/page.tsx
+++ src/app/(app)/file/[type]/[id]/[name]/page.tsx
@@ -1,7 +1,11 @@
+import EmptySection from '@/components/Sections/AppSections/Errors/EmptySection';
 import PDFViewer from './_components/PdfViewer';
 import { getFiles } from '@/lib/queries/queries';
 import { QueryFileProps } from '@/types/globals';
+import Image from 'next/image';
 import React from 'react'
+import { IconFaceIdError } from '@tabler/icons-react';
+import Header from './_components/Header';
 
 interface PageProps {
   params: QueryFileProps;
@@ -14,7 +18,35 @@ export async function generateStaticParams() {
 const page: React.FC<PageProps> = async ({ params: { id, name, type } }) => {
   const data = await getFiles({ id, name, type });
 
-  return <PDFViewer base64String={data[type]} />;
+  if (data.ERROR) {
+    return (
+      <EmptySection
+        reload
+        Icon={IconFaceIdError}
+        title="Ocurrió un error al obtener datos"
+        description="Hubo un problema al procesar la solicitud. Por favor, verifica tu conexión e intenta nuevamente."
+      />
+    );
+  }
+
+  if (data.ContentType === "application/pdf") {
+    return <PDFViewer base64String={data[type]} />;
+  }
+
+  return (
+    <div className="flex flex-col h-dvh">
+      <Header />
+      <div className="flex flex-1 w-full justify-center items-center">
+        <Image
+          alt={id}
+          src={`data:${data.ContentType};base64,${data[type]}`}
+          width={500}
+          height={500}
+          className="h-auto max-h-[calc(100dvh-104px)] object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page

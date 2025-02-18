eb61cdd2a0eb53f78f98b140b908aa81c6765f5c
Santino Degra <91171834+SAX2@users.noreply.github.com>
2025-02-18 13:35:44 -0500

    Update urls.ts

--- src/lib/queries/urls.ts
+++ src/lib/queries/urls.ts
@@ -1,11 +1,13 @@
-export const urlControlPanel = "/cer_exp.nsf/xsp/.xrest/panel";
-export const urlUnits = process.env.BASE_API_URL  + "/cer_exp.nsf/xsp/.xrest/panel";
-export const urlUnit = process.env.BASE_API_URL  + "/cer_exp.nsf/xsp/.xrest/unidad";
-export const urlUnitIssues = process.env.BASE_API_URL  + "/cer_ied.nsf/xsp/.xrest/rclList";
-export const urlUnitReservations = process.env.BASE_API_URL  + "/cer_rec.nsf/xsp/.xrest/recListEdificio";
-export const urlUnitPayments = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/notiPagoList";
-export const urlUser = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/usuario";
-export const urlFile = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/adjunto";
-export const urlToken = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/token";
-export const urlCreateNotifyPayment = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/notificarPago";
-export const urlCreateNewRcl = process.env.BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/altaRcl";
+let BASE_API_URL = "https://www.consorciosenred.com/cerDevelopment"
+
+export const urlControlPanel = BASE_API_URL + "/cer_exp.nsf/xsp/.xrest/panel";
+export const urlUnits = BASE_API_URL  + "/cer_exp.nsf/xsp/.xrest/panel";
+export const urlUnit = BASE_API_URL  + "/cer_exp.nsf/xsp/.xrest/unidad";
+export const urlUnitIssues = BASE_API_URL  + "/cer_ied.nsf/xsp/.xrest/rclList";
+export const urlUnitReservations = BASE_API_URL  + "/cer_rec.nsf/xsp/.xrest/recListEdificio";
+export const urlUnitPayments = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/notiPagoList";
+export const urlUser = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/usuario";
+export const urlFile = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/adjunto";
+export const urlToken = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/token";
+export const urlCreateNotifyPayment = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/notificarPago";
+export const urlCreateNewRcl = BASE_API_URL  + "/cer_api.nsf/xsp/.xrest/altaRcl";

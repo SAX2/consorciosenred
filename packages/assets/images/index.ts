b2d694b6189904b7284ca33aa157717cec432dc5
SAX2 <santinodegra73@gmail.com>
2025-04-08 17:08:46 -0300

    Hace arreglos en imagenes

--- packages/assets/images/index.ts
+++ packages/assets/images/index.ts
@@ -1,8 +1,5 @@
 import Logo from "../../../public/logo/consorciosenred.svg";
 import LogoWhite from "../../../public/logo/consorciosenredwhite.svg";
-import CABA1 from "../../../public/images/obelisco_atardecer_9_de_julio_1500x610.webp";
-import CABA2 from "../../../public/images/CABA2.webp";
-import CABA3 from "../../../public/images/CABA3.webp";
 import DesktopAdminPreview from "../../../public/images/desktop-admin-preview.webp";
 
 import MobileAppInstallPreview from "../../../public/images/app-install-preview-iphone.webp";
@@ -13,21 +10,12 @@ import MobileUnit from "../../../public/images/unit-mobile.webp";
 import MobileUnits from "../../../public/images/units-mobile.png";
 
 import Pattern from "../../../public/utils/pattern1.svg";
-// import UserImage1 from "../../public/utils/user-image/user-image-1.png";
-// import UserImage2 from "../../public/utils/user-image/user-image-2.png";
-import SisadmImage from "../../../public/utils/sisadm-img.jpg";
 
 export const logoBackgroundBlue = Logo;
 export const logoBackgroundWhite = LogoWhite;
-export const caba1 = CABA1;
-export const caba2 = CABA2;
-export const caba3 = CABA3;
 export const pattern = Pattern;
 export const desktopAdminPreview = DesktopAdminPreview;
 export const mobileAppInstallPreview = MobileAppInstallPreview;
-// export const userImage1 = UserImage1;
-// export const userImage2 = UserImage2;
-export const sisAdmImg = SisadmImage;
 
 export const mobileRcl = MobileRcl;
 export const mobileUnit = MobileUnit;
